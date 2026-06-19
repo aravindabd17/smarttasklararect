799a89b9106a9e8fbc30e481b04b7328d5f15786
aravindabd17 <aravind@buymediaspace.com>
2026-06-19 14:50:03 +0530

    first commit

---
+++ smart-task-manager-api/app/Http/Controllers/UserController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $search=request('search');
+        $users=User::when($search,function($query,$search){
+            $query->where('name','like',"%{$search}%")
+            ->orWhere('email','like',"%{$search}%");
+        })
+        ->latest()
+        ->paginate(10);
+        
+        return UserResource::collection($users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $user=User::create([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(User $user)
+    {
+        return new UserResource($user);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        
+        $user->update([
+            'name'=>$request->name,
+            'email'=>$request->email
+        ]);
+
+        return new UserResource($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json([
+            'message'=>'User record deleted'
+        ]);
+    }
+}
